c7d36f7ea3aaf69f81ec77b88b9d68a7f37d6f4a
vanshaj-c0dz <vanshajsharma033@gmail.com>
2026-03-24 10:39:37 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app  = express()
+
+const PORT = process.env.PORT || 3000
+
+
+app.get('/' , (req,res)=>{
+    res.status(200).json('Hello from Acquisitions!')
+})
+
+export default app
